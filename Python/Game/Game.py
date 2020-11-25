0ef16a59d2b631072be50032cf1f29a171da3082
molu-guwang <250289332@qq.com>
2020-11-25 21:28:03 +0800

    study

--- Python/Game/Game.py
+++ Python/Game/Game.py
@@ -35,11 +35,11 @@ Shoes = {
 
 Treasures = ('Helmet', 'Clothes', 'Weapon', 'Pants', 'Rings', 'Shoes')
 Treasures_Own = []
-Hero_Initial_Characteristic = {'HP': 10, 'MP': 5, 'Attack': 4, 'Defense': 1, 'EXP': 0, 'Level': 1,
-                               'Crit_Probability': 50, 'Miss_Probability': 0, 'Move_Speed': 1}
+Hero_Initial_Characteristic = {'HP': 10, 'MP': 5, 'Attack': 4, 'Defense': 1, 'EXP': 19, 'Level': 1,
+                               'Crit_Probability': 4, 'Miss_Probability': 0, 'Move_Speed': 1}
 
 Monster_List = {}
-Monster_Characteristic = {'HP': 200, 'MP': 6, 'Attack': 20, 'Defense': 1, 'EXP': 3, 'Level': 2,
+Monster_Characteristic = {'HP': 40, 'MP': 6, 'Attack': 20, 'Defense': 1, 'EXP': 3, 'Level': 2,
                           'Crit_Probability': 0, 'Miss_Probability': 1}
 
 # 英雄属性为初始属性加上装备属性
@@ -49,9 +49,12 @@ Hero_Characteristic = {
     'Defense': int(Hero_Initial_Characteristic['Defense']) + int(Helmet['Defense']) + int(Clothes['Defense']) + int(
         Pants['Defense']),
     'Attack': int(Hero_Initial_Characteristic['Attack']) + int(Weapon['Attack']),
-    'Crit_Probability': int(Rings['Crit_Probability']),
-    'Miss_probability': int(Shoes['Miss_probability']),
-    'Move_Speed': int(Hero_Initial_Characteristic['Move_Speed']) + int(Shoes['Move_Speed'])
+    'Crit_Probability': int(Hero_Initial_Characteristic['Crit_Probability']) + int(Rings['Crit_Probability']),
+    'Miss_probability': int(Hero_Initial_Characteristic['Miss_Probability']) + int(Shoes['Miss_probability']),
+    'Move_Speed': int(Hero_Initial_Characteristic['Move_Speed']) + int(Shoes['Move_Speed']),
+    'EXP_Have': int(Hero_Initial_Characteristic['EXP']),
+    'EXP_Ceiling': 20
+
 }
 
 print(Hero_Characteristic)
@@ -81,7 +84,6 @@ def Fight():
             Monster_HP_Remind = Monster_Characteristic['HP'] + Monster_Characteristic['Defense'] - Hero_Characteristic[
                 'Attack']
             Hero_HP_Remind = Hero_Characteristic['HP_Remind']
-
         Monster_Characteristic['HP'] = Monster_HP_Remind
         Hero_Characteristic['HP_Remind'] = Hero_HP_Remind
         randrange(1, 100)
@@ -92,6 +94,10 @@ def Fight():
     else:
         if Monster_HP_Remind < 0:
             print('You are winning')
+            Hero_Characteristic['EXP_Have'] = int(Hero_Initial_Characteristic['EXP']) + int(Monster_Characteristic[
+                                                                                                'EXP'])
+            print(Hero_Characteristic['EXP_Have'])
+            Level_Up()
         elif Hero_HP_Remind < 0:
             print('You are death')
 
@@ -107,7 +113,27 @@ def Player_Choice():
         print("Make a choose")
 
 
-Choice = input("please choose you choise:"
+# 升级
+def Level_Up():
+    if Hero_Characteristic['EXP_Have'] > int(Hero_Characteristic['EXP_Ceiling']):
+        print('Level up!')
+        Hero_Initial_Characteristic['EXP'] = int(Hero_Characteristic['EXP_Have']) - int(Hero_Characteristic[
+                                                                                            'EXP_Ceiling'])
+        Hero_Initial_Characteristic['Level'] += 1
+        Hero_Characteristic['EXP_Ceiling'] += 30
+        Hero_Initial_Characteristic['Attack'] += 2
+        Hero_Initial_Characteristic['Defense'] += 1
+        Hero_Initial_Characteristic['HP'] += 3
+        Hero_Initial_Characteristic['MP'] += 1
+        Hero_Initial_Characteristic['Move_Speed'] += 0.25
+        Hero_Characteristic['HP_Remind'] = Hero_Characteristic['HP_Max']
+        print(Hero_Initial_Characteristic)
+        print(Hero_Characteristic)
+
+
+Choice = input("please choose you choice:"
                "fight"
                "run")
 Player_Choice()
+
+
