54ec0560582418b4bc76b377b946dc682e4e6fd0
molu-guwang <250289332@qq.com>
2023-02-20 22:14:32 +0800

    study

--- Python/study/study.py
+++ Python/study/study.py
@@ -14,13 +14,6 @@ print('----------')
 List = [1, 2, 3, 4]
 it = iter(List)  # 创建迭代器对象
 
-while True:
-    try:
-        print(next(it))
-    except StopIteration:
-        sys.exit()
-
-sys.path
 
 '''
 class Animal():
