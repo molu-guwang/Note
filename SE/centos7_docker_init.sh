304548958ceea957579ac5fa1c7ffad2a81fdafd
25028 <250289332@qq.com>
2023-10-16 17:14:41 +0800

    test

--- SE/centos7_docker_init.sh
+++ SE/centos7_docker_init.sh
@@ -8,19 +8,17 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 curl  https://get.docker.com | bash -s docker --mirror Aliyun
 yum install -y yum-utils   device-mapper-persistent-data   lvm2
 yum-config-manager     --add-repo     http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io -y --allowerasing
+yum install docker-ce docker-ce-cli containerd.io -y
 systemctl start docker
 echo '{
 
-    "registry-mirrors":["https://almtd3fa.mirror.aliyuncs.com"]
+    "registry-mirrors":["https://almtd3fa.mirror.aliyuncs.com"],
+    "data-root":"/home/data/docker/"
+
 
 }' >> /etc/docker/daemon.json
 systemctl stop docker
-mkdir -p  /home/data/docker
-mkdir -p /etc/systemd/system/docker.service.d/
-echo " [Service]
-ExecStart=
-ExecStart=/usr/bin/dockerd  --graph=/home/data/docker/ " >> /etc/systemd/system/docker.service.d/devicemapper.conf
+mkdir -p  /home/data/docker/mount
 systemctl daemon-reload
 systemctl restart docker
 systemctl enable docker
